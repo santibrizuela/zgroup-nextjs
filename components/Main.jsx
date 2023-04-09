08a0895e9aa7807b62e16708ff57125b40cee2f1
santibrizuela <santibrizuela.97@gmail.com>
2023-04-09 08:18:57 -0300

    0904230818 commit

--- components/Main.jsx
+++ components/Main.jsx
@@ -9,11 +9,11 @@ const Main = () => {
   return (
     <div id='home' className='w-full h-screen text-center mask'>
         <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
-            <div>
+            <div className='justify-center text-center'>
                 <p className='uppercase text-sm tracking-widest text-gray-100 font-bold'>ASISTENCIA AL INMIGRANTE</p>
-                <div>
-                    <Image className='rounded-xl scale-90 hover:scale-100 ease-in duration-300' src={contactImg} alt="Contact" />
-                </div>
+                
+                    <Image className='rounded-xl' src={contactImg} alt="Contact" />
+                
                 <h2 className='py-2 text-gray-300'>Tu camino comienza aquí.</h2>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4 text-gray-300'>
                     <Link target='_blank' href='https://www.whatsapp.com/'>
